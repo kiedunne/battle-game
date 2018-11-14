2762d85650effa592a91fec341a0f9e75d6c6999
kiedunne <kie.dunne@gmail.com>
2018-11-14 18:21:11 +0000

    Skinny controller- tests passing

--- app.rb
+++ app.rb
@@ -12,21 +12,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player1 = Player.new(params[:player1])
-    $player2 = Player.new(params[:player2])
+    player1 = Player.new(params[:player1])
+    player2 = Player.new(params[:player2])
+    $game = Game.new(player1, player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player1_name = $player1.name
-    @player2_name = $player2.name
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $player1
-    @player2 = $player2
-    Game.new(@player1, @player2).attack(@player2)
+    @game = $game
+    @game.attack(@game.player2)
     erb :attack
   end
 
